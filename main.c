2d6e7fb3cffb2d745763190ef7d6fa15562739b8
Taylor Davis <td916215@ohio.edu>
2018-09-14 08:37:47 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 14;
+    int y = x + 3;
+    int z = y + x;
+
+    //printf("Hello world!\n");
+
+    printf("%d, %d, %d",x,y,z);
+    return 0;
+}
